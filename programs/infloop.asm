6a756327a24656117e74297911dd939122a45b67
Dante Falzone <dante.j.falzone@protonmail.com>
2019-09-30 11:05:33 -0500

    Added enlightening comments

--- programs/infloop.asm
+++ programs/infloop.asm
@@ -1,14 +1,16 @@
-movl    ra      15
-movl    rb      0xEEEE ; E
-push    rb
-dec     ra
-jeq     ra rc   0x0014
-jmp     0x0008
+movl    ra      15     ; counter for how many times to push onto stack
+movl    rb      0xEEEE ; value to push onto the stack
+push    rb             ; push it
+dec     ra             ; decrement the counter
+jeq     ra rc   0x0014 ; if counter == 0, goto pop operation below
+jmp     0x0008         ; else, go back up to push operation
 
-pop     rb
-inc     ra
-movl    rd      15
-jeq     ra rd   0x0008
-jmp     0x0014
+pop     rb             ; take a byte off
+inc     ra             ; increment counter
+movl    rd      15     ; prepare this value for comparison
+jeq     ra rd   0x0008 ; if counter == 15, goto push operation above
+jmp     0x0014         ; else, go back to pop operation
 
-; no halt needed; this program runs indefinitely
+; No halt needed; this program runs indefinitely.
+; Enjoy watching it fill and empty your stack over
+; and over again until you pkill or ^C the vm.
