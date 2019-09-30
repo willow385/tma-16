fcba3f71a8ee534991a87f94cd876f18e4e55819
Dante Falzone <dante.j.falzone@protonmail.com>
2019-09-30 11:39:24 -0500

    Made use of the new macro directive "#define"

--- programs/mult.asm
+++ programs/mult.asm
@@ -1,42 +1,38 @@
 ; A program written in TMA-16 assembly that multiplies two
-; numbers: 3 and 7, resulting in 21.
+; numbers defined by macro constants. I wrote it to multiply
+; 3 by 7 but you can change that by editing the macros below.
+#define MULT_OPERAND_0      3
+#define MULT_OPERAND_1      7
+#define EXPECTED_RESULT    21
 
-movl    ra   3             ; first operand
-movl    rb   7             ; second operand
-movr    rc   ra            ; copy the original value of the first operand
-movl    rd   1             ; we'll compare against this to see if we're done
+; don't change these two
+#define ADD_LOOP_START 0x000F ; address of the `add' operation
+#define STRING_PRINT   0x0027 ; address where we start printing to stdout
 
-add     ra   rc            ; address of this op is at 0x000F
-dec     rb                 ; decrement the counter
-jgr     rb   rd     0x000F ; if 1 > rb then goto add operation
+movl    ra    MULT_OPERAND_0  ; first operand
+movl    rb    MULT_OPERAND_1  ; second operand
+movr    rc    ra              ; copy the original value of the first operand
+movl    rd    1               ; we'll compare against this to see if we're done
+
+add     ra    rc              ; we'll repeat this to perform the multiplication
+dec     rb                    ; decrement the counter
+jgr     rb rd ADD_LOOP_START  ; if 1 > rb then goto add operation
 
 
-movl    rd   21            ; now to compare and see if we got the right answer
-jeq     rd   ra     0x0027 ; ra == 21 ? goto 0x0027 : halt
+movl    rd    EXPECTED_RESULT ; now to compare and see if we got the right answer
+jeq     rd ra STRING_PRINT    ; ra == EXPECTED_RESULT ? goto STRING_PRINT : halt
 
-halt                       ; if ra != 21 then we exit here
+halt                       ; if ra != EXPECTED_RESULT then we exit here
 
-; if we got here, then it must mean that ra contains the value 21, and that
-; our program therefore successfully multiplied 3 by 7
-movl    rd   51            ; ascii '3'
+; if we got here, then it must mean that ra contains the EXPECTED_RESULT, and that
+; our program therefore successfully multiplied MULT_OPERAND_0 by MULT_OPERAND_1
+movl    rd   68            ; 'D'
 out     rd
-movl    rd   32            ; ascii space
+movl    rd   79            ; 'O'
 out     rd
-movl    rd   42            ; ascii asterisk
+movl    rd   78            ; 'N'
 out     rd
-movl    rd   32
-out     rd
-movl    rd   55            ; ascii '7'
-out     rd
-movl    rd   32
-out     rd
-movl    rd   61            ; ascii '='
-out     rd
-movl    rd   32
-out     rd
-movl    rd   50            ; ascii '2'
-out     rd
-movl    rd   49            ; ascii '1'
+movl    rd   69            ; 'E'
 out     rd
 movl    rd   10            ; newline
 out     rd
