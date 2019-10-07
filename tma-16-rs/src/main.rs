d78f9553f9e7f16751566a624b54c8f4b7cc1cc9
Dante Falzone <dante.j.falzone@protonmail.com>
2019-10-07 11:13:56 -0500

    Wrote function to read a file as a Vec<u8> in the Rust implementation

--- tma-16-rs/src/main.rs
+++ tma-16-rs/src/main.rs
@@ -1,5 +1,8 @@
 use std::process;
 use std::env;
+use std::fs::File;
+use std::fs;
+use std::io::Read;
 
 // Function to get the name of the executable from command-line args.
 fn get_tmx_name(args: Vec<String>) -> Result<String, &'static str> {
@@ -24,6 +27,20 @@ fn contains_poor_option(args: Vec<String>) -> bool {
     false
 }
 
+// Function to read the contents of a binary file up to 64KB in size.
+fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
+    let mut f = File::open(&filename).expect("no file found");
+    let metadata = fs::metadata(&filename).expect("unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("buffer overflow");
+
+    if metadata.len() > 0xFFFF {
+        panic!("Oversized executable");
+    }
+
+    buffer
+}
+
 // Function to stop the TMA-16 in various scenarios.
 fn hardware_exception(error_msg: String) {
     println!("Error! {}", error_msg);
@@ -31,14 +48,25 @@ fn hardware_exception(error_msg: String) {
     process::exit(1);
 }
 
+
+
 fn main() -> Result<(), String> {
     let args_vec: Vec<String> = env::args().collect();
 
     /* never thought I'd be using `.map_err(|e| e.to_string())?` with a
        function I wrote myself lol */
     let tmx_filename = get_tmx_name(args_vec).map_err(|e| e.to_string())?;
+    let file_buf: Vec<u8> = get_file_as_byte_vec(&tmx_filename);
 
     println!("{}", tmx_filename);
+    for i in file_buf {
+        if i < 0x10 {
+            println!("0{:x?}", i);
+        } else {
+            println!("{:x?}", i);
+        }
+    }
+
 
     Ok(())
 }
