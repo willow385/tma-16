80828bc9a3ba8862795b6385ed3b38914a5ddede
Dante Falzone <dante.j.falzone@protonmail.com>
2019-10-07 12:26:57 -0500

    Implemented `movl()` for struct Tma16

--- tma-16-rs/src/machine.rs
+++ tma-16-rs/src/machine.rs
@@ -38,7 +38,39 @@ impl Tma16 {
             0x0C => Ok(self.rc),
             0x0D => Ok(self.rd),
             0x0E => Ok(self.ip),
-            _ => Err(hardware_exception("nonexistent register".to_string())),
+            _ => Err(
+                    hardware_exception(
+                        format!("nonexistent register {:x?}", num)
+                    .to_string()
+                    )
+                )
+        }
+    }
+
+    pub fn movl(&mut self, reg: u8, value: u16) -> Result<(), ()> {
+        match reg {
+            0x0A => {
+                self.ra = value;
+                Ok(())
+            },
+            0x0B => {
+                self.rb = value;
+                Ok(())
+            },
+            0x0C => {
+                self.rc = value;
+                Ok(())
+            },
+            0x0D => {
+                self.rd = value;
+                Ok(())
+            },
+            _ => Err(
+                    hardware_exception(
+                        format!("illegal instruction operand {:x?}", reg)
+                    .to_string()
+                    )
+                )
         }
     }
 }
